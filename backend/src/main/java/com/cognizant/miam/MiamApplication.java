6062f10b0fc712b48332e443946134333fefe4af
mmi947221 <maryvon.michel@cognizant.com>
2021-07-23 08:32:38 +0200

    Init project

---
+++ backend/src/main/java/com/cognizant/miam/MiamApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.miam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cognizant/miam/MiamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.miam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
