e9a0491e61132a25d33e7d904aaa57ce8b74efb5
Maryvon MICHEL <mmaryvon@outlook.com>
2021-07-27 13:03:16 +0200

    add docker-compose: config

---
+++ backend/src/main/java/com/cognizant/miam/persitence/model/Project.java
@@ -0,0 +1,20 @@
+package com.cognizant.miam.persitence.model;
+
+import java.time.LocalDate;
+
+public class Project {
+	/**
+	 * The id technique of project
+	 */
+	private Long id;
+
+	/**
+	 *  The name of the project
+	 */
+    private String name;
+
+    /**
+     *  Creation date of project
+     */
+    private LocalDate dateCreated;
+}
