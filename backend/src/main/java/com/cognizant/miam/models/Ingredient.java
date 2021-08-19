4526164fec8939b8483418d37f1d1d8fd10eb502
Moulay Ahmed EL IDRISSI ATTACH <ahmed.elidrissi.attach@gmail.com>
2021-08-19 18:53:29 +0200

    [MIAM-61] Image dynamically

--- backend/src/main/java/com/cognizant/miam/models/Ingredient.java
+++ backend/src/main/java/com/cognizant/miam/models/Ingredient.java
@@ -1,13 +1,17 @@
 package com.cognizant.miam.models;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,14 +21,15 @@ import java.util.Set;
 @Table(name = "ingredient")
 public class Ingredient {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name ="id")
-  private long id;
-  @NotNull
-  @NotBlank
-  @Column(name ="name")
-  private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "name")
+    private String name;
 
     @OneToMany(mappedBy = "ingredient")
     private Set<RecipeIngredient> recipes = new HashSet<>();
