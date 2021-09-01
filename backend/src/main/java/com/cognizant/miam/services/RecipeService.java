9cee7b6a3745f8a4060c10f5c9ba7677f5c04092
Antoine Rouillier <43577206+Veinozor8047@users.noreply.github.com>
2021-09-01 11:40:36 +0200

    [MIAM-42] Modifying the number of people in a dish from the grocery list

--- backend/src/main/java/com/cognizant/miam/services/RecipeService.java
+++ backend/src/main/java/com/cognizant/miam/services/RecipeService.java
@@ -5,7 +5,7 @@ import com.cognizant.miam.dto.RecipeDTO;
 import java.util.List;
 
 public interface RecipeService {
-    public RecipeDTO save(RecipeDTO recipe);
+    RecipeDTO save(RecipeDTO recipe);
 
     List<RecipeDTO> findAll();
 
---
+++ frontend/src/app/global/grocery-list/grocery-list.component.ts
@@ -0,0 +1,81 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faShoppingCart, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { GroceryList } from "../../models/grocery-list";
+import { Ingredient } from "../../models/ingredient";
+import { Recipe } from "../../models/recipe";
+
+@Component({
+  selector: 'app-grocery-list',
+  templateUrl: './grocery-list.component.html',
+  styleUrls: ['./grocery-list.component.scss']
+})
+export class GroceryListComponent implements OnInit {
+  faShoppingCart = faShoppingCart
+  faTrash = faTrash
+
+  @Input() headerMode = true
+  popupVisible: boolean = false
+
+  groceryList: GroceryList = { name: "Ma liste de courses", ingredients: [], dishes: [] }
+  sessionStorageKey = "GroceryList-" + new Date().getDate() + "/" + new Date().getMonth()
+
+  addIngredient(ingredient: Ingredient, quantity: number) {
+    this.groceryList.ingredients.push({ ingredient, quantity })
+    this.saveGrocerySession()
+  }
+
+  addDish(recipe: Recipe, quantity: number) {
+    this.groceryList.dishes.push({ recipe, quantity })
+    this.saveGrocerySession()
+  }
+
+  showPopup() {
+    this.popupVisible = !this.popupVisible
+  }
+
+  emptyList() {
+    this.groceryList.ingredients = []
+    this.groceryList.dishes = []
+    this.saveGrocerySession()
+  }
+
+  removeDish(dish: any) {
+    if (confirm("Êtes-vous sûr de vouloir supprimer ce repas?")) {
+      this.groceryList.dishes.splice(this.groceryList.dishes.indexOf(dish), 1)
+    }
+  }
+
+  minusDishQuantity(dish: any) {
+    if (dish.quantity > 1) {
+      dish.quantity--
+    } else if (dish.quantity == 1) {
+      this.removeDish(dish)
+    }
+    this.saveGrocerySession()
+  }
+
+  plusDishQuantity(dish: any) {
+    dish.quantity++
+    this.saveGrocerySession()
+
+  }
+  saveGrocerySession() {
+    sessionStorage.setItem(this.sessionStorageKey, JSON.stringify(this.groceryList))
+  }
+
+  getGrocerySession() {
+    let localGroceryList = sessionStorage.getItem(this.sessionStorageKey)
+    if (!localGroceryList) {
+      this.saveGrocerySession()
+    } else {
+      this.groceryList = JSON.parse(localGroceryList)
+    }
+  }
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.getGrocerySession()
+  }
+}
