911002528cee18349d9d1d52210dbc4bd1c637a3
AkiraRatta <rattanatray.akira@gmail.com>
2021-09-02 11:18:32 +0200

    [MIAM-68] Dockerisation

--- backend/src/main/java/com/cognizant/miam/configurations/DataSourceConfig.java
+++ backend/src/main/java/com/cognizant/miam/configurations/DataSourceConfig.java
@@ -14,46 +14,28 @@ import java.util.Properties;
 
 @Configuration
 public class DataSourceConfig {
-    @Autowired
-    private Environment env;
     private DataSourceBuilder dataSourceBuilder;
-    private Properties prop = null;
-    private String osName = "";
 
     @PostConstruct
     public void setup(){
-        prop = new Properties();
         dataSourceBuilder = DataSourceBuilder.create();
-        osName= System.getProperty("os.name");
-        String repoUser = env.getProperty("HOME");
-
-        if((osName.toUpperCase()).startsWith("WINDOWS")) {
-            repoUser = env.getProperty("USERPROFILE");
-        }
-
-        try (InputStream input = new FileInputStream(repoUser +"/.miam-project.properties")) {
-            prop = new Properties();
-            prop.load(input);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
     }
 
     @Bean
     @Profile("dev")
     public DataSource getDevDataSource() {
-        dataSourceBuilder.url(prop.getProperty("PG_URL"));
-        dataSourceBuilder.username(prop.getProperty("PG_USERNAME"));
-        dataSourceBuilder.password(prop.getProperty("PG_PASSWORD"));
+        dataSourceBuilder.url(System.getenv("PG_URL"));
+        dataSourceBuilder.username(System.getenv("PG_USERNAME"));
+        dataSourceBuilder.password(System.getenv("PG_PASSWORD"));
         return dataSourceBuilder.build();
     }
 
     @Bean
     @Profile("prod")
     public DataSource getProdDataSource() {
-        dataSourceBuilder.url(env.getProperty("PG_URL"));
-        dataSourceBuilder.username(env.getProperty("PG_USERNAME"));
-        dataSourceBuilder.password(env.getProperty("PG_PASSWORD"));
+        dataSourceBuilder.url(System.getenv("PG_URL"));
+        dataSourceBuilder.username(System.getenv("PG_USERNAME"));
+        dataSourceBuilder.password(System.getenv("PG_PASSWORD"));
         return dataSourceBuilder.build();
     }
 }
