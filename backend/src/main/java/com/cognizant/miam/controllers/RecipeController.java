0c2ac7054605823dc92aa6ebbc73462c2354f3e2
JavaProScript <71082092+JavaProScript@users.noreply.github.com>
2021-09-06 10:45:26 +0200

    [MIAM-42] modifying dishes quantity in grocery list

--- backend/src/main/java/com/cognizant/miam/controllers/RecipeController.java
+++ backend/src/main/java/com/cognizant/miam/controllers/RecipeController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
 @RestController
 @RequestMapping(value = "/recipe")
 @CrossOrigin(origins = "http://localhost:4200")
